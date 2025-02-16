ee7326c43159c4cac5c46e0b9cfbefe23ac6d6ef
abhishek rao <abhishekrao@abhisheks-MacBook-Pro.local>
2025-02-16 15:06:45 +0530

    About section

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from 'react'
 import React from 'react'
 import Home from './components/Home/Home'
-
+import About from './components/About/About'
 
 function App() {
 
   return (
     <>
+    
       <Home />
+      <About />
  
     </>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,12 @@ import "./index.css"
 import App from './App.jsx'
 import { createBrowserRouter, createRoutesFromElements, RouterProvider, Route } from 'react-router-dom'
 import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
 
 
 const route = createBrowserRouter(
   createRoutesFromElements(
-    <Route path = "/" element = {<Home />}>
+    <Route path = "/" element = {<About />}>
       <Route path=''></Route>
     </Route>
   )
