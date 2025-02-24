54be5231c85cd378608c4f808543638f7ff71cd0
Divyansh Mishra <divyansh.72003@gmail.com>
2025-02-24 11:51:47 +0530

    Minor Changes

--- src/components/Home/Footer.jsx
+++ src/components/Home/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <footer className="bg-[#FFCC6D] py-6 flex justify-center relative z-1 mt-[-50px] h-1/6 shadow-md"> 
       <div className="flex space-x-8 text-gray-800 font-semibold">
         <span className="text-lg">Hero 1</span>
-        <span className="text-xl font-bold">Hero 2</span>
+        <span className="text-xl font-bold">Amit Trivedi</span>
         <span className="text-lg">Hero 3</span>
       </div>
     </footer>
