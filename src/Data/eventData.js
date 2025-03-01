8789f7a408a2b59d4939f5260d85a9ba1ae8e069
Vinay Kumar Dubey <Vinay22573@iiitd.ac.in>
2025-03-02 02:43:16 +0530

    Events Page changes in progress:Vinay22573

---
+++ src/Data/eventData.js
@@ -0,0 +1,291 @@
+const events = [
+    {
+        "event_name": "RASIK",
+        "organising_club": "Tasveer",
+        "vertical": "PhotoGraphy",
+        "link": "https://unstop.com/p/pixie-pixels-indraprastha-institute-of-information-technology-iiit-delhi-1347370",
+        "description": "A captivating photography event showcasing the best of visual storytelling."
+    },
+    {
+        "event_name": "DRISHYAM",
+        "organising_club": "Tasveer",
+        "vertical": "PhotoGraphy",
+        "link": "https://unstop.com/p/drishyam-indraprastha-institute-of-information-technology-iiit-delhi-1348274",
+        "description": "An event to capture the essence of life through the lens."
+    },
+    {
+        "event_name": "RANG DE REEL",
+        "organising_club": "Tasveer",
+        "vertical": "PhotoGraphy",
+        "link": "https://unstop.com/p/flashback-frames-indraprastha-institute-of-information-technology-iiit-delhi-1348254",
+        "description": "A vibrant photography competition celebrating colors and creativity."
+    },
+    {
+        "event_name": "VIGYAPAN",
+        "organising_club": "Tasveer",
+        "vertical": "PhotoGraphy",
+        "link": "https://unstop.com/p/the-ad-renaline-rush-indraprastha-institute-of-information-technology-iiit-delhi-1348265",
+        "description": "Showcase your advertising skills through compelling photographs."
+    },
+    {
+        "event_name": "SRIJAN",
+        "organising_club": "Tasveer",
+        "vertical": "PhotoGraphy",
+        "link": "https://unstop.com/p/dreamscape-indraprastha-institute-of-information-technology-iiit-delhi-1348289",
+        "description": "A platform to exhibit your creative photography talent."
+    },
+    {
+        "event_name": "Encore Western (Western Solo Singing)",
+        "organising_club": "Audiobytes",
+        "vertical": "Music",
+        "link": "https://unstop.com/o/8K2zZ5E?lb=NLds0R8a",
+        "description": "A solo singing competition featuring western music genres."
+    },
+    {
+        "event_name": "Arrpegio",
+        "organising_club": "Audiobytes",
+        "vertical": "Music",
+        "link": "https://unstop.com/o/rSX5ZUB?lb=tDOoeHN2",
+        "description": "An event to showcase your musical talent through beautiful melodies."
+    },
+    {
+        "event_name": "Encore Indian (Bollywood Solo Singing)",
+        "organising_club": "Audiobytes",
+        "vertical": "Music",
+        "link": "https://unstop.com/p/encore-indian-hindi-solo-singing-odyssey25-indraprastha-institute-of-information-technology-iiit-delhi-1398606",
+        "description": "A solo singing competition featuring Bollywood music."
+    },
+    {
+        "event_name": "Battle of Bands",
+        "organising_club": "Audiobytes",
+        "vertical": "Music",
+        "link": "https://unstop.com/o/IQzS8de?lb=Tvl9YDCg",
+        "description": "A thrilling competition where bands battle it out for the top spot."
+    },
+    {
+        "event_name": "Anime Karaoke",
+        "organising_club": "Audiobytes",
+        "vertical": "Music",
+        "link": "https://unstop.com/p/anime-karaoke-no-yoru-2025-odyssey25-indraprastha-institute-of-information-technology-iiit-delhi-1405480",
+        "description": "Sing your favorite anime songs in this fun karaoke event."
+    },
+    {
+        "event_name": "Acapella",
+        "organising_club": "Audiobytes",
+        "vertical": "Music",
+        "link": "https://unstop.com/p/acapella-odyssey-odyssey25-indraprastha-institute-of-information-technology-iiit-delhi-1405295",
+        "description": "A competition showcasing the art of acapella singing."
+    },
+    {
+        "event_name": "Hasratein",
+        "organising_club": "Machaan",
+        "vertical": "Theater",
+        "link": "https://unstop.com/p/hasratein-street-play-odyssey25-indraprastha-institute-of-information-technology-iiit-delhi-1405237",
+        "description": "A street play event highlighting social issues and human emotions."
+    },
+    {
+        "event_name": "Befikr",
+        "organising_club": "Machaan",
+        "vertical": "Theater",
+        "link": "Befikr - The Stage Play - 2025 | 1404405 // Unstop",
+        "description": "A stage play event that brings stories to life with powerful performances."
+    },
+    {
+        "event_name": "Improv Fiesta",
+        "organising_club": "Machaan",
+        "vertical": "Theater",
+        "link": "https://unstop.com/p/improv-fiesta-odyssey25-indraprastha-institute-of-information-technology-iiit-delhi-1405449?lb=PPcLvIhx&utm_medium=Share&utm_source=shortUrl",
+        "description": "An improvisational theater event where creativity meets spontaneity."
+    },
+    {
+        "event_name": "OneAct: Spotlight Edition",
+        "organising_club": "Machaan",
+        "vertical": "Theater",
+        "link": "OneAct: Spotlight Edition - The Mono Acting Spectacle! - 2025 | 1404294 // Unstop",
+        "description": "A mono-acting competition that showcases individual talent and storytelling."
+    },
+    {
+        "event_name": "Anime T SHirts",
+        "organising_club": "Meraki",
+        "vertical": "Art",
+        "link": "https://unstop.com/p/anime-t-shirt-odyssey25-indraprastha-institute-of-information-technology-iiit-delhi-1398507",
+        "description": "Design your favorite anime characters on T-shirts in this creative event."
+    },
+    {
+        "event_name": "Only Ink",
+        "organising_club": "Meraki",
+        "vertical": "Art",
+        "link": "https://unstop.com/p/only-ink-odyssey25-indraprastha-institute-of-information-technology-iiit-delhi-1398505",
+        "description": "An art competition focusing on the beauty of ink drawings."
+    },
+    {
+        "event_name": "Reverb ",
+        "organising_club": "Muse",
+        "vertical": "Fashion",
+        "link": "https://unstop.com/p/reverb-2025-indraprastha-institute-of-information-technology-iiit-delhi-1348360",
+        "description": "A fashion show event that brings the latest trends to the runway."
+    },
+    {
+        "event_name": "Trashion ",
+        "organising_club": "Muse",
+        "vertical": "Fashion",
+        "link": "https://unstop.com/p/trashion-2025-iiitd-indraprastha-institute-of-information-technology-1347265",
+        "description": "A unique fashion event where participants create outfits from recycled materials."
+    },
+    {
+        "event_name": "Comic con Cosplay (Muse X LDA )",
+        "organising_club": "Muse",
+        "vertical": "Fashion",
+        "link": "https://unstop.com/p/dress-note-iiit-delhi-1348010",
+        "description": "A cosplay competition celebrating comic book and anime characters."
+    },
+    {
+        "event_name": "Glam Up",
+        "organising_club": "Muse",
+        "vertical": "Fashion",
+        "link": "https://unstop.com/o/4LZP7QY?lb=DmNmxfSf&utm_medium=Share&utm_source=shortUrl",
+        "description": "A glamorous fashion event showcasing stunning makeup and styling."
+    },
+    {
+        "event_name": "battle of chefs",
+        "organising_club": "Salt'n Pepper",
+        "vertical": "Culinary",
+        "link": "https: //unstop.com/p/battle-of-chefs-20-odyssey25-iiitd-1405112",
+        "description": "A culinary competition where chefs battle it out to create the best dishes."
+    },
+    {
+        "event_name": "Food For Feud",
+        "organising_club": "Salt'n Pepper",
+        "vertical": "Culinary",
+        "link": "https: //unstop.com/events/food-for-fued-odyssey25-iiitd-indraprastha-institute-of-information-technology-1405232",
+        "description": "A fun and competitive food event where participants showcase their culinary skills."
+    },
+    {
+        "event_name": "Verve",
+        "organising_club": "MadToes",
+        "vertical": "Dance",
+        "link": "https://unstop.com/o/eJ8hSsC?lb=iZQuQ3Ug&utm_medium=Share&utm_source=shortUrl",
+        "description": "A high-energy dance competition featuring various dance styles."
+    },
+    {
+        "event_name": "Battle Troupe",
+        "organising_club": "MadToes",
+        "vertical": "Dance",
+        "link": "https://unstop.com/o/LjeKGlQ?lb=iZQuQ3Ug&utm_medium=Share&utm_source=shortUrl",
+        "description": "A dance battle event where troupes compete for the top spot."
+    },
+    {
+        "event_name": "Bidbuzz ",
+        "organising_club": "Finnexia",
+        "vertical": "Finance & ENT",
+        "link": "Superhero Showdown - 2025 | 1391856 // Unstop",
+        "description": "A finance event where participants bid on various items and investments."
+    },
+    {
+        "event_name": "Pirates of the stock market ",
+        "organising_club": "Finnexia",
+        "vertical": "Finance & ENT",
+        "link": "https://unstop.com/p/pirates-of-the-stock-market-odyssey25-iiitd-indraprastha-institute-of-information-technology-iiit-delhi-1391698",
+        "description": "A stock market simulation event where participants trade and invest."
+    },
+    {
+        "event_name": "Poker",
+        "organising_club": "Finnexia",
+        "vertical": "Finance & ENT",
+        "link": "POKER - 2025 | 1394804 // Unstop",
+        "description": "A poker tournament where participants compete for the winning hand."
+    },
+    {
+        "event_name": "Black Jack",
+        "organising_club": "Finnexia",
+        "vertical": "Finance & ENT",
+        "link": "https://unstop.com/p/blackjack-bonanza-odyssey25-indraprastha-institute-of-information-technology-iiit-delhi-1402932",
+        "description": "A blackjack competition where participants aim to beat the dealer."
+    },
+    {
+        "event_name": "IPL Auction ",
+        "organising_club": "Finnexia",
+        "vertical": "Finance & ENT",
+        "link": "https://unstop.com/competitions/ipl-auction-odyssey25-iiitd-iiit-delhi-1396960",
+        "description": "An auction event where participants bid on IPL players and teams."
+    },
+    {
+        "event_name": "Shark Tank",
+        "organising_club": "Finnexia",
+        "vertical": "Finance & ENT",
+        "link": "https://unstop.com/p/finnexia-x-odysseys-shark-tank-iiit-delhi-odyssey25-indraprastha-institute-of-information-technology-iiit-delhi-1402978",
+        "description": "A business pitch event where participants present their ideas to investors."
+    },
+    {
+        "event_name": "Critical Bid",
+        "organising_club": "Finnexia",
+        "vertical": "Finance & ENT",
+        "link": "Critical Bid - 2025 | 1402930 // Unstop",
+        "description": "A bidding competition where participants strategize to win critical items."
+    },
+    {
+        "event_name": "storage wars",
+        "organising_club": "Finnexia",
+        "vertical": "Finance & ENT",
+        "link": "https://unstop.com/o/fm9QdqC?lb=MEVB5Zn&utm_medium=Share&utm_source=shortUrl",
+        "description": "A storage auction event where participants bid on storage units."
+    },
+    {
+        "event_name": "Uno",
+        "organising_club": "Finnexia",
+        "vertical": "Finance & ENT",
+        "link": "https://unstop.com/events/uno-odyssey25-indraprastha-institute-of-information-technology-iiit-delhi-1403003",
+        "description": "A fun and competitive Uno card game tournament."
+    },
+    {
+        "event_name": "Period Pain Simulator ///holy cramp",
+        "organising_club": "Girl Up Udaan",
+        "vertical": "Women EmpowerMent Events",
+        "link": "https://mstats.dare2compete.com/CL0/https:%2F%2Funstop.com%2Fp%2Fholy-cramp-odyssey25-iiitd-indraprastha-institute-of-information-technology-iiit-delhi-1395783%3Futm_campaign=site-emails%26utm_medium=d2c-automated%26utm_source=you-did-it-invite-only-mode-activated-for-holy-cramp/1/01090194ebfdf422-4abce7f7-55ed-401b-b8a0-193489055fb9-000000/uNiytsxLj9dUBsJVlO9q_RaHSThlVpXc_WXrRaodmic=193",
+        "description": "An event to raise awareness about period pain through a simulator experience."
+    },
+    {
+        "event_name": "Smash the paintriarchy ",
+        "organising_club": "Girl Up Udaan",
+        "vertical": "Women EmpowerMent Events",
+        "link": "https://unstop.com/p/smash-the-paint-riarchy-odyssey25-iiitd-indraprastha-institute-of-information-technology-iiit-delhi-1394476?utm_campaign=site-emails&utm_medium=d2c-automated&utm_source=you-did-it-invite-only-mode-activated-for-smash-the-paint-riarchy",
+        "description": "A painting event focused on breaking stereotypes and empowering women."
+    },
+    {
+        "event_name": "Ctrl+Alt+Laugh",
+        "organising_club": "Women in Tech",
+        "vertical": "Women EmpowerMent Events",
+        "link": "https://unstop.com/events/tech-roast-a-comedy-show-odyssey25-iiitd-indraprastha-institute-of-information-technology-iiit-delhi-1391456",
+        "description": "A comedy show featuring tech-related jokes and roasts."
+    },
+    {
+        "event_name": "SnapSolve",
+        "organising_club": "Women in Tech x Girl Up Udaan",
+        "vertical": "Women EmpowerMent Events",
+        "link": "https://unstop.com/competitions/snapsolve-odyssey25-indraprastha-institute-of-information-technology-iiit-delhi-1397079",
+        "description": "A problem-solving competition focused on women's empowerment issues."
+    },
+    {
+        "event_name": "Sorry Not Sorry",
+        "organising_club": "LDA",
+        "vertical": "Literature",
+        "link": "https://unstop.com/o/Ktl1WBk?lb=9hCEtB7H&utm_medium=Share&utm_source=shortUrl",
+        "description": "A literature event where participants write unapologetic and bold pieces."
+    },
+    {   
+        "event_name": "ESPN Junction",
+        "organising_club": "Triviallis",
+        "vertical": "Quiz Sc-fi Tech",
+        "link": "https://unstop.com/p/es-pn-junction-a-sports-sci-tech-quiz-odyssey25-indraprastha-institute-of-information-technology-iiit-delhi-1405481",
+        "description": "Test your knowledge in sports, science, and tech — challenge yourself!"
+    },
+    {
+        "event_name": "Sports and Entertainment Quiz",
+        "organising_club": "Trivialis",
+        "vertical": "Quiz Event",
+        "link": "https://unstop.com/quiz/es-pn-junction-a-sports-sci-tech-quiz-odyssey25-indraprastha-institute-of-information-technology-iiit-delhi-1405481"
+    }
+];
+
+
+export default events;
--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -1,30 +1,43 @@
 import React from "react";
-import Navbar from "../Home/Navbar";
 import logo from "../../assets/odyssey.png";
 import text from "../../assets/text.png";
 import footer from "../../assets/FOOTER.png";
-
-const EventCard = () => (
-  <article
-    className="text-white p-6 w-full sm:w-80 lg:w-96 rounded-3xl shadow-lg flex flex-col items-center transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-105"
-    style={{ background: "#A03232" }}
-  >
+import events from "../../Data/eventData";
+import "./event.css";
+const EventCard = ({ event }) => (
+  <article className="text-white p-6 w-full sm:w-80 lg:w-96 rounded-3xl shadow-lg flex flex-col items-center transition duration-300 ease-in-out hover:-translate-y-1 hover:scale-105 bg-[#A03232] relative z-10">
     <div className="w-full h-48 rounded-3xl bg-black mb-4"></div>
-    <h3 className="p-3 text-3xl font-bold">EVENT NAME</h3>
-    <p className="text-m font-semibold text-left">
-      Description Description Description Description Description
-    </p>
-    <button className="mt-4 text-lg md:text-xl w-full bg-[#FFCC6D] px-4 py-2 font-bold rounded-3xl text-[#A03232]">
-      REGISTER
-    </button>
+    <article className="flex flex-col items-center flex-start gap-2 w-full">
+      <h3 className="p-3 pb-1 text-4xl  font-bold text-center tracking-widest font-custom">
+        {event.event_name}
+      </h3>
+      <p className="flex flex-row gap-2 flex-wrap">
+        <span className="text-sm  border-[#FFCC6D] border-2 text-[#FFCC6D] font-normal text-center px-2 py-0.5 rounded-full flex flex-row items-center gap-1">
+          <img src="src/assets/Club.svg" alt="Club-Icon" className="w-6 h-6" />{" "}
+          {event.organising_club}
+        </span>
+        <span className="text-sm  border-[#FFCC6D] border-2 text-[#FFCC6D] font-normal text-center px-2 py-0.5 rounded-full">
+          {event.vertical}
+        </span>
+      </p>
+
+      <p className="text-sm md:text-base text-center font-normal px-2 py-0.5 rounded-full flex flex-row gap-1">
+        {event.description}
+      </p>
+    </article>
+    <a href={event.link} className="w-full">
+      <button className="mt-4 text-2xl md:text-3xl tracking-wider w-full bg-[#FFCC6D] px-4 py-2 font-bold rounded-3xl text-[#A03232] font-custom">
+        REGISTER
+      </button>
+    </a>
   </article>
 );
 
 const EventsSection = () => (
   <section className="text-center py-8">
     <div className="flex flex-wrap justify-center gap-6 py-6 px-4 mx-auto max-w-[1500px]">
-      {[...Array(9)].map((_, index) => (
-        <EventCard key={index} />
+      {events.map((event, index) => (
+        <EventCard key={index} event={event} />
       ))}
     </div>
   </section>
@@ -33,24 +46,38 @@ const EventsSection = () => (
 export default function Home() {
   return (
     <>
-      <div className="bg-[#FDFEC4] min-h-screen">
-        
-        <div className="h-5/6 relative flex flex-col items-center">
-          <img src={logo} alt="Logo" className="w-80 mx-auto mb-4" />
-          <img
-            src={text}
-            alt="Event Text"
-            className="p-6 mx-auto mb-4 animate-slideFromBottom"
-            style={{ width: "38rem" }}
-          />
+      <section className="bg-[#FDFEC4] min-h-screen relative">
+        <div className="relative">
+          <div className="absolute top-0 justify-between w-full hidden xl:flex">
+            <img
+              src="src/assets/event_top_left.svg"
+              alt="decorative item1"
+              className="relative left-0 w-96"
+            />
+            <img
+              src="src/assets/event_top_right.svg"
+              alt="decorative item2"
+              className="relative right-0 w-96"
+            />
+          </div>
+          <div className="h-5/6 relative flex flex-col items-center">
+            <img src={logo} alt="Logo" className="w-80 mx-auto mb-4" />
+            <img
+              src={text}
+              alt="Event Text"
+              className="p-6 mx-auto mb-4 animate-slideFromBottom"
+              style={{ width: "38rem" }}
+            />
+          </div>
+          <EventsSection />
+          <div className="absolute bottom-0 right-0 translate-y-10 z-5">
+            <img src="src/assets/event_castle.svg" alt="" />
+          </div>
         </div>
-        <EventsSection />
-        <footer
-          className="w-full h-32 flex justify-center items-center bg-[#A03232]"
-        >
+        <footer className="w-full h-32 flex justify-center items-center bg-[#A03232] rounded-t-3xl relative z-10">
           <img src={footer} alt="Footer Logo" className="w-40 mx-auto" />
         </footer>
-      </div>
+      </section>
     </>
   );
 }
