9bec030a2eba138eba43c93d6d45b7e901b01f1e
Vinay Kumar Dubey <Vinay22573@iiitd.ac.in>
2025-03-02 18:23:42 +0530

    Trying new About Page:Vinay22573

--- src/main.jsx
+++ src/main.jsx
@@ -8,6 +8,7 @@ import Layout from './components/Layout/Layout.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import Register from './components/Register/Register.jsx'
 import Try from './components/Try/Try.jsx'
+import About from './components/Abt/abt.jsx'
 const route = createBrowserRouter(
   createRoutesFromElements(
     <>
@@ -17,6 +18,8 @@ const route = createBrowserRouter(
       <Route path = "/contact" element={<Contact />} />
       <Route path = "/register" element={<Register />} />
       <Route path = "/try" element={<Try/>} />
+      <Route path = "/try" element={<Try/>} />
+      <Route path = "/Abt" element={<About/>} />
       </Route>
     </>
   )
