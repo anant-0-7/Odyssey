eeb21bb22c2f132a4c30e6175354e4751cd84d33
Vinay Kumar Dubey <Vinay22573@iiitd.ac.in>
2025-03-07 22:56:13 +0530

    Added Avlbl-Team Members Data:Vinay22573

---
+++ src/Data/teamData.js
@@ -0,0 +1,193 @@
+import TeamImages from "../assets/Team_Images";
+const teams = [
+    {
+        "person_image": TeamImages.Vipul_Mishra,
+        "person_name": "Vipul Mishra",
+        "link_1": "https://github.com/vipul1009",
+        "link_2": "https://www.instagram.com/v_vipul_/",
+        "link_3": "www.linkedin.com/in/vipulmishra22596",
+        "vertical": "Events",
+        "designation": "OC"
+    },
+    {
+        "person_image": "",
+        "person_name": "Neelu",
+        "link_1": "https://github.com/Neeluiiitd",
+        "link_2": "https://odyssey.iiitd.edu.in",
+        "link_3": "https://odyssey.iiitd.edu.in",
+        "vertical": "Events",
+        "designation": "OC"
+    },
+    {
+        "person_image": TeamImages.shubhi_jain,
+        "person_name": "Shubhi Jain",
+        "link_1": "https://github.com/j-shubhi",
+        "link_2": "https://www.instagram.com/being_shubhi_?igsh=MTV3NHVwbDZrbHVnMw==",
+        "link_3": "https://www.linkedin.com/in/j-shubhi/",
+        "vertical": "Design",
+        "designation": "OC"
+    },
+    {
+        "person_image": "",
+        "person_name": "Neelanjan Sarkar",
+        "link_1": "https://odyssey.iiitd.edu.in",
+        "link_2": "https://odyssey.iiitd.edu.in",
+        "link_3": "www.linkedin.com/in/neelanjansarkar",
+        "vertical": "Coverage",
+        "designation": "OC"
+    },
+    {
+        "person_image": "",
+        "person_name": "Rijusmit Biswas",
+        "link_1": "https://github.com/riju-talk",
+        "link_2": "https://www.instagram.com/beyond_the_celestials/",
+        "link_3": "https://www.linkedin.com/in/rijusmit-biswas/",
+        "vertical": "Development",
+        "designation": "Lead"
+    },
+    {
+        "person_image": "",
+        "person_name": "Aditya Dahiya",
+        "link_1": "https://github.com/adityad171",
+        "link_2": "https://www.instagram.com/_adityadahiya?igsh=MTN3bnI1MHczNmQ4cw==",
+        "link_3": "https://www.linkedin.com/in/aditya-dahiya-3ba285229/",
+        "vertical": "Events",
+        "designation": "OC"
+    },
+    {
+        "person_image": "",
+        "person_name": "Anmol Gupta",
+        "link_1": "https://github.com/Ag-Anmol",
+        "link_2": "https://www.instagram.com/itz.anmolg/",
+        "link_3": "linkedin.com/in/anmol-gupta-95983526a/",
+        "vertical": "Ambience",
+        "designation": "OC"
+    },
+    {
+        "person_image": TeamImages.pranshu_goel,
+        "person_name": "Pranshu Goel",
+        "link_1": "https://github.com/Pranshu369",
+        "link_2": "https://www.instagram.com/pranshuu.goel/",
+        "link_3": "https://www.linkedin.com/in/pranshu-goel-942608257/",
+        "vertical": "Ambience",
+        "designation": "OC"
+    },
+    {
+        "person_image": TeamImages.Vibhav,
+        "person_name": "Vibhav",
+        "link_1": "https://odyssey.iiitd.edu.in",
+        "link_2": "https://www.instagram.com/vibhavzzz?igsh=dTczYTQxNDQyYzVi",
+        "link_3": "https://www.linkedin.com/in/vibhavbalhara?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+        "vertical": "Operations",
+        "designation": "OC"
+    },
+    {
+        "person_image": TeamImages.Nootan_Sharma,
+        "person_name": "Nootan sharma",
+        "link_1": "https://odyssey.iiitd.edu.in",
+        "link_2": "https://www.instagram.com/ntn_here_?igsh=aXlmb2gyMG9yczh6",
+        "link_3": "https://www.linkedin.com/in/nootan-sharma-65002b25a?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+        "vertical": "Operations",
+        "designation": "OC"
+    },
+    {
+        "person_image": "",
+        "person_name": "Dharmesh",
+        "link_1": "https://odyssey.iiitd.edu.in",
+        "link_2": "https://www.instagram.com/dharmesh.xd?igsh=MWZyMGI5Y2RzdXE5dA==",
+        "link_3": "https://www.linkedin.com/in/dharmender-kumar-76b3a52a6?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+        "vertical": "PR",
+        "designation": "OC"
+    },
+    {
+        "person_image": "",
+        "person_name": "Amit",
+        "link_1": "https://odyssey.iiitd.edu.in",
+        "link_2": "https://www.instagram.com/amit_beniwal_04?igsh=Znk4ajk4eHhkYWhl&utm_source=qr",
+        "link_3": "https://odyssey.iiitd.edu.in",
+        "vertical": "Operations",
+        "designation": "Lead"
+    },
+    {
+        "person_image": TeamImages.Satvik_Arya,
+        "person_name": "Satvik Arya",
+        "link_1": "https://odyssey.iiitd.edu.in",
+        "link_2": "https://www.instagram.com/satvik__sagwal?igsh=ZmZsMGpsOXNhYW1h",
+        "link_3": "https://www.linkedin.com/in/satvik-sagwal-800180274?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+        "vertical": "Operations",
+        "designation": "Lead"
+    },
+    {
+        "person_image": TeamImages.Lakshya_Verma,
+        "person_name": "Lakshya Verma",
+        "link_1": "https://odyssey.iiitd.edu.in",
+        "link_2": "https://www.instagram.com/_lakshya.verma_?igsh=YXNraDFqeHplMGJr",
+        "link_3": "https://www.linkedin.com/in/lakshya-verma-935b5a29b?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app",
+        "vertical": "Operations",
+        "designation": "Lead"
+    },
+    {
+        "person_image": "",
+        "person_name": "Jaideep Dahiya",
+        "link_1": "https://github.com/mad-guitaristJD",
+        "link_2": "https://www.instagram.com/jaideepdahiya21?igsh=ZjZ0OWk3N2t4dzRt",
+        "link_3": "https://www.linkedin.com/in/jaideep-dahiya-587836312?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app",
+        "vertical": "Operations",
+        "designation": "Lead"
+    },
+    {
+        "person_image": "",
+        "person_name": "Sankit",
+        "link_1": "https://github.com/Sankit2512",
+        "link_2": "https://www.instagram.com/sankitchoudhary_07/?hl=en",
+        "link_3": "https://www.linkedin.com/in/sankit-choudhary-94a27a258/",
+        "vertical": "Operations",
+        "designation": "Lead"
+    },
+    {
+        "person_image": TeamImages.Bagadi_Abhinav,
+        "person_name": "Bagadi Abhinav",
+        "link_1": "https://github.com/abhinavbagadi05",
+        "link_2": "https://www.instagram.com/abhinav_bagadi/",
+        "link_3": "https://www.linkedin.com/in/abhinav-bagadi-16691623a/",
+        "vertical": "Ambience",
+        "designation": "Lead"
+    },
+    {
+        "person_image": TeamImages.Akanksha_Ray,
+        "person_name": "Akanksha Ray",
+        "link_1": "https://github.com/akanksha23056",
+        "link_2": "https://www.instagram.com/rayakanksha_/",
+        "link_3": "https://www.linkedin.com/in/akanksha-ray-a577b0289/",
+        "vertical": "ambience",
+        "designation": "Lead"
+    },
+    {
+        "person_image": "",
+        "person_name": "ayaan hasan",
+        "link_1": "https://odyssey.iiitd.edu.in",
+        "link_2": "https://www.instagram.com/ayaan__1813?igsh=MTJldGZ5cDRlaDlvdQ==",
+        "link_3": "https://www.linkedin.com/in/ayaan-hasan-72b5a0258/",
+        "vertical": "operations",
+        "designation": "Lead"
+    },
+    {
+        "person_image": "",
+        "person_name": "Shubham Goel",
+        "link_1": "https://github.com/shubham22490",
+        "link_2": "https://www.instagram.com/_.imshubham._/",
+        "link_3": "https://www.linkedin.com/in/shubhamgoel2802/",
+        "vertical": "ambience",
+        "designation": "Lead"
+    },
+    {
+        "person_image": TeamImages.Vinay_Kumar_Dubey,
+        "person_name": "Vinay Kumar Dubey",
+        "link_1": "https://github.com/vinay22573",
+        "link_2": "https://www.instagram.com/vinay22573/",
+        "link_3": "https://www.linkedin.com/in/vinay-kumar22573/",
+        "vertical": "Development",
+        "designation": "Developer"
+    }
+];
+export default teams;
---
+++ src/assets/Team_Images/index.js
@@ -0,0 +1,24 @@
+
+import Akanksha_Ray from './Akanksha Ray.jpg'
+import Bagadi_Abhinav from './Bagadi_Abhinav.jpg'
+import Lakshya_Verma from './Lakshya_Verma.jpg'
+import Nootan_Sharma from './Nootan_Sharma.jpg'
+import pranshu_goel from './pranshu_goel.jpg'
+import Satvik_Arya from './Satvik_Arya.jpg'
+import shubhi_jain from './shubhi_jain.jpg'
+import Vibhav from './Vibhav.jpg'
+import Vinay_Kumar_Dubey from './Vinay_Kumar_Dubey.jpg'
+import Vipul_Mishra from './Vipul_Mishra.jpg'
+const TeamImages = { 
+    Akanksha_Ray,
+    Bagadi_Abhinav,
+    Lakshya_Verma,
+    Nootan_Sharma,
+    pranshu_goel,
+    Satvik_Arya,
+    shubhi_jain,
+    Vibhav,
+    Vinay_Kumar_Dubey,
+    Vipul_Mishra
+}
+export default TeamImages;
